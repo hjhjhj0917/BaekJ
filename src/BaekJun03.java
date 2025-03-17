05dc14e65e4f000f5c55a31ec8dd10868747c5f6
hjhjhj0917 <abc22842199@gmail.com>
2025-03-17 11:29:56 +0900

    과제 1일차(3)

---
+++ src/BaekJun03.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class BaekJun03 {
+
+    // 두 정수 A와 B를 입력받은 다음, A/B를 출력하는 프로그램을 작성하시오.
+
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("값을 입력하시오 : ");
+        int A = sc.nextInt();
+
+        System.out.println("값을 입력하시오 : ");
+        int B = sc.nextInt();
+
+        System.out.println(A / B);
+
+    }
+}
