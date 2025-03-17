a6384a564e6f20e7648a2f658bea6e5990c3a897
hjhjhj0917 <abc22842199@gmail.com>
2025-03-17 11:08:20 +0900

    과제 1일차(2)

---
+++ src/BaekJun02.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class BaekJun02 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("값을 입력하시오: ");
+        int A = sc.nextInt();
+
+        System.out.println("값을 입력하시오: ");
+        int B = sc.nextInt();
+
+        System.out.println(A - B);
+    }
+}
