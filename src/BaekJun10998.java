5fa5b63e58a64839ef71641a02d2487eb25b3707
hjhjhj0917 <abc22842199@gmail.com>
2025-03-18 10:00:06 +0900

    과제 2일차(3)

---
+++ src/BaekJun10998.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class BaekJun10998 {
+    public static void main(String[] args){
+
+        // 두 정수 A와 B를 입력받은 다음, A*B를 출력하는 프로그램을 작성하시오.
+
+        Scanner sc = new Scanner(System.in);
+
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+
+        System.out.println(a * b);
+    }
+}
