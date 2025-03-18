37f743409d182830a1914f593977c155263b2b02
hjhjhj0917 <abc22842199@gmail.com>
2025-03-18 09:59:11 +0900

    과제 1일차(2)

---
+++ src/BaekJun1001.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class BaekJun1001 {
+
+    // 두 정수 A와 B를 입력받은 다음, A-B를 출력하는 프로그램을 작성하시오.
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        // System.out.println("값을 입력하시오: ");
+        int a = sc.nextInt();
+
+        // System.out.println("값을 입력하시오: ");
+        int b = sc.nextInt();
+
+        System.out.println(a - b);
+
+    }
+}
