91a71b6442919be96b3ff6587df1e5baaf29749e
hjhjhj0917 <abc22842199@gmail.com>
2025-03-21 08:41:53 +0900

    과제 5일차(2)

---
+++ src/BaekJun10171.java
@@ -0,0 +1,14 @@
+public class BaekJun10171 {
+
+    //     \    /\
+    //      )  ( ')
+    //     (  /  )
+    //      \(__)|      위 같은 그림을 출력하시오.
+
+    public static void main(String[] args){
+
+        System.out.println("\\    /\\" + System.lineSeparator() + " )  ( ')"
+                + System.lineSeparator() + "(  /  )"
+                + System.lineSeparator() + " \\(__)|");
+    }
+}
