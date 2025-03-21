8a036c5edd83ec06d1fd7839d125def1ba94d35a
hjhjhj0917 <abc22842199@gmail.com>
2025-03-21 14:28:23 +0900

    과제 1일차(1)

--- src/BackJun1000.java
+++ src/BackJun1000.java
@@ -1,5 +1,4 @@
-import java.util.Scanner;
-                          // java.util 패키지에 있는 Scanner함수를 가져옴
+import java.util.Scanner;  // java.util 패키지에 있는 Scanner 클래스를 가져옴
 
 public class BackJun1000 {
 
@@ -7,13 +6,13 @@ public class BackJun1000 {
 
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);  // 사용자에게 값을 입력 받기 위해서 Scanner함수를 사용함
+        Scanner sc = new Scanner(System.in);  // 사용자에게 값을 입력 받기 위해서 Scanner를 사용함
 
         // System.out.println("값을 입력하시오 : ");
-        int a = sc.nextInt();  // Scanner 함수에서 입력 받은 값을 int를 사용하여 정수형태로 변수 A에 넣음
+        int a = sc.nextInt();  // Scanner에서 입력 받은 값을 int를 사용하여 정수형태로 변수 A에 넣음
 
         // System.out.println("값을 입력하시오 : ");
-        int b = sc.nextInt();  // Scanner 함수에서 입력 받은 값을 int를 사용하여 정수형태로 변수 B에 넣음
+        int b = sc.nextInt();  // Scanner 에서 입력 받은 값을 int를 사용하여 정수형태로 변수 B에 넣음
 
         System.out.println(a + b);  // 입력 받은 정수값을 + 로 연산하여 출력함
     }
