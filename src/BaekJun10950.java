b8b9a4c509ee453e62f06eafa9ee200766c9349f
hjhjhj0917 <abc22842199@gmail.com>
2025-03-27 17:41:31 +0900

    25.03.27

--- src/BaekJun10950.java
+++ src/BaekJun10950.java
@@ -2,6 +2,8 @@ import java.util.Scanner;
 
 public class BaekJun10950 {
 
+    // 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+
     public static void main(String[] args){
 
         Scanner sc = new Scanner(System.in);
