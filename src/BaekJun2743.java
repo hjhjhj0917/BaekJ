963f5657d3327d54a6833ac44a4cb5244557d0d8
hjhjhj0917 <abc22842199@gmail.com>
2025-04-03 08:41:40 +0900

    25.04.03

---
+++ src/BaekJun2743.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class BaekJun2743 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String line = sc.nextLine().trim();
+        String[] str = line.split("");
+
+        System.out.println(str.length);
+    }
+}
