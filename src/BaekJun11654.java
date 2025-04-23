dbfc859953072c04cba3da2512e71aad815711c1
hjhjhj0917 <abc22842199@gmail.com>
2025-04-24 08:39:44 +0900

    25.04.24

---
+++ src/BaekJun11654.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class BaekJun11654 {
+
+    // 알파벳 소문자, 대문자, 숫자 0-9중 하나가 주어졌을 때, 주어진 글자의 아스키 코드값을 출력하는 프로그램을 작성하시오.
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        char ch = sc.nextLine().charAt(0);
+        int aci = (int) ch;
+
+        System.out.println(aci);
+    }
+}
