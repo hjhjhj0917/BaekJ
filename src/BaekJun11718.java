bb5137ce9f4cb9ac90c51179d67aa8c29806dfa8
hjhjhj0917 <abc22842199@gmail.com>
2025-04-24 08:39:58 +0900

    25.04.24

---
+++ src/BaekJun11718.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class BaekJun11718 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        while (sc.hasNextLine()) {
+
+            String line = sc.nextLine();
+            System.out.println(line);
+        }
+        sc.close();
+    }
+}
