ff3ff279f549d15f8700d46406f733ecbecd6e77
hjhjhj0917 <abc22842199@gmail.com>
2025-04-30 14:38:20 +0900

    25.04.30

---
+++ src/BaekJun25311.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class BaekJun25311 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int year = sc.nextInt();
+
+        System.out.println("A");
+    }
+}
