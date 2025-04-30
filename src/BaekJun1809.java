a6cf3e3c2ef9c1701fe100ed88028679dbd9c51b
hjhjhj0917 <abc22842199@gmail.com>
2025-04-30 09:45:51 +0900

    25.04.30

---
+++ src/BaekJun1809.java
@@ -0,0 +1,11 @@
+public class BaekJun1809 {
+    public static void main(String[] args) {
+
+        System.out.println("(___)");
+        System.out.println("(o o)____/");
+        System.out.println(" @@      \\");
+        System.out.println("  \\ ____,/");
+        System.out.println("  //   //");
+        System.out.println(" ^^   ^^");
+    }
+}
