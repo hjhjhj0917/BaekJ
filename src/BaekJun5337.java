dd91c661d2fe4c102f7c8c7a6804e1d736c6053b
hjhjhj0917 <abc22842199@gmail.com>
2025-05-12 09:42:15 +0900

    25.05.12

---
+++ src/BaekJun5337.java
@@ -0,0 +1,8 @@
+public class BaekJun5337 {
+    public static void main(String[] args) {
+
+        System.out.printf(".  .   .\n" +
+                          "|  | _ | _. _ ._ _  _\n" +
+                          "|/\\|(/.|(_.(_)[ | )(/.");
+    }
+}
