ed4adf51d001425053d10987dc045e8bca351d07
hjhjhj0917 <abc22842199@gmail.com>
2025-05-15 08:52:27 +0900

    25.05.15

---
+++ src/Baekjun31429.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Baekjun31429 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int rank = sc.nextInt();
+
+        if (rank == 1) {
+            System.out.println("12 1600");
+        } else if (rank == 2) {
+            System.out.println("11 894");
+        } else if (rank == 3) {
+            System.out.println("11 1327");
+        } else if (rank == 4) {
+            System.out.println("10 1311");
+        } else if (rank == 5) {
+            System.out.println("9 1004");
+        } else if (rank == 6) {
+            System.out.println("9 1178");
+        } else if (rank == 7) {
+            System.out.println("9 1357");
+        } else if (rank == 8) {
+            System.out.println("8 837");
+        } else if (rank == 9) {
+            System.out.println("7 1055");
+        } else if (rank == 10) {
+            System.out.println("6 556");
+        } else if (rank == 11) {
+            System.out.println("6 773");
+        }
+    }
+}
