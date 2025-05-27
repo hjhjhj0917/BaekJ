1e2cafebd6d30ad98013a8b82d018062a883e33e
hjhjhj0917 <abc22842199@gmail.com>
2025-05-28 08:56:27 +0900

    25.05.28

---
+++ src/BaekJun5339.java
@@ -0,0 +1,15 @@
+public class BaekJun5339 {
+    public static void main(String[] args) {
+
+        System.out.printf("     /~\\\n" +
+                "    ( oo|\n" +
+                "    _\\=/_\n" +
+                "   /  _  \\\n" +
+                "  //|/.\\|\\\\\n" +
+                " ||  \\ /  ||\n" +
+                "============\n" +
+                "|          |\n" +
+                "|          |\n" +
+                "|          |");
+    }
+}
