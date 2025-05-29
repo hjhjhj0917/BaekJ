f2296402e97d5b0e91f4d145770f7154471e53d3
hjhjhj0917 <abc22842199@gmail.com>
2025-05-30 08:50:03 +0900

    25.05.30

---
+++ src/BaekJun33515.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class BaekJun33515 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int time1 = sc.nextInt();
+        int time2 = sc.nextInt();
+
+        System.out.println(Math.min(time1, time2));
+    }
+}
