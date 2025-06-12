cfb97b53c7d407660681212cbcaa56b4d9885941
hjhjhj0917 <abc22842199@gmail.com>
2025-06-13 08:48:03 +0900

    25.06.13

---
+++ src/BaekJun21598.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class BaekJun21598 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+
+        for (int i = 0; i < num; i++) {
+            System.out.println("SciComLove");
+        }
+    }
+}
