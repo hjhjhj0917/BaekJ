fada0255de13b1a256edb95656929437311d1daa
hjhjhj0917 <abc22842199@gmail.com>
2025-06-24 09:12:41 +0900

    25.06.24

---
+++ src/BaekJun10189.java
@@ -0,0 +1,9 @@
+public class BaekJun10189 {
+    public static void main(String[] args) {
+
+        System.out.print("#  # #### #### #  #\n" +
+                "#### #  # #  # # #\n" +
+                "#### #  # #  # # #\n" +
+                "#  # #### #### #  #");
+    }
+}
