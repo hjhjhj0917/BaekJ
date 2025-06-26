c7376969ddf19fff072011a89354d1821f9f23b0
hjhjhj0917 <abc22842199@gmail.com>
2025-06-27 08:47:43 +0900

    25.06.27

---
+++ src/BaekJun2741.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class BaekJun2741 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int numb = sc.nextInt();
+
+        for (int i = 1; i <= numb; i++) {
+            System.out.println(i);
+        }
+    }
+}
