235b3a0e51ea2c3a097ef70cf6009638731bd47e
hjhjhj0917 <abc22842199@gmail.com>
2025-07-21 14:06:10 +0900

    25.07.21

---
+++ src/BaekJun14645.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class BaekJun14645 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+        int man = sc.nextInt();
+
+        for (int i = 0; i < num; i++) {
+            int ride = sc.nextInt();
+            int down = sc.nextInt();
+        }
+        System.out.println("비와이");
+    }
+}
