55d3602a8e3c3fdd8c03ffac9dd0d8b56d9507b8
hjhjhj0917 <abc22842199@gmail.com>
2025-07-24 10:06:31 +0900

    25.07.24

---
+++ src/BaekJun15680.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class BaekJun15680 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+
+        switch (num) {
+            case 0:
+                System.out.println("YONSEI");
+                break;
+            case 1:
+                System.out.println("Leading the Way to the Future");
+                break;
+            default:
+                break;
+        }
+    }
+}
