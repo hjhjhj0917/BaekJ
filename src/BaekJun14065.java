40d145e2aaaf762231ac1f5df15ee36dd695861c
hjhjhj0917 <abc22842199@gmail.com>
2025-09-17 13:10:18 +0900

    25.09.17

---
+++ src/BaekJun14065.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class BaekJun14065 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double num = sc.nextDouble();
+        double x = 235.214583;
+
+        System.out.println(x / num);
+    }
+}
