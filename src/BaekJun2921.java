a4b5455fe110aced92a2a4500a00956c1bc55ce2
data8316-17 <yjmo0309@gmail.com>
2026-01-20 14:54:42 +0900

    26.01.20

---
+++ src/BaekJun2921.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class BaekJun2921 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+        int increase = 0;
+        int result = 0;
+
+        for (int i = 0; i < num; i ++) {
+
+            increase += 3 + (3 * i);
+            result += increase;
+        }
+
+        System.out.println(result);
+    }
+}
